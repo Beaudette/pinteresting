636730a8e70369fb76047b199b0cfcc2e9152b4c
Blake Beaudette <beaudette@gmail.com>
2015-03-21 13:21:49 -0700

    added jquery masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
   # Required for Heroku
   # Note to set this to actual host
   config.action_mailer.default_url_options = { host: 'blake-pinteresting.herokuapp.com' }
+
 end
